ba998230b3e4486540d683fe9ae96f5ad23d0016
hwangjiyoung02 <hjy020714@gmail.com>
2025-06-28 15:13:04 +0900

    chore: 초기 세팅 (1)

---
+++ src/main/java/com/leets/chikahae/ChikahaeBeApplication.java
@@ -0,0 +1,13 @@
+package com.leets.chikahae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChikahaeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChikahaeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leets/chikahae/ChikahaeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leets.chikahae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChikahaeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
