27a89f76d98a0160b081d99a6aa33d41dbb2763d
hwangjiyoung02 <hjy020714@gmail.com>
2025-07-07 11:14:33 +0900

    feat: 공통 엔티티 추가, 스웨거 config 설정

--- src/main/java/com/leets/chikahae/ChikahaeBeApplication.java
+++ src/main/java/com/leets/chikahae/ChikahaeBeApplication.java
@@ -2,8 +2,10 @@ package com.leets.chikahae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ChikahaeBeApplication {
 
 	public static void main(String[] args) {
