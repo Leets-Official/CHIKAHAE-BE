4f3433ce3f81aaf38cb52a52dec0aac92ce42977
seokjun01 <suckjungod@naver.com>
2025-07-08 21:45:41 +0900

    feat: 알림 관련 설정파일 임시 추가

---
+++ src/main/java/com/leets/chikahae/global/config/FcmConfig.java
@@ -0,0 +1,6 @@
+package com.leets.chikahae.global.config;
+
+public class FcmConfig {
+
+	//Firebase 관련 환경설정 파일
+}
---
+++ src/main/java/com/leets/chikahae/global/config/SchedulingConfig.java
@@ -0,0 +1,6 @@
+package com.leets.chikahae.global.config;
+
+public class SchedulingConfig {
+
+	// 스케줄 관련 설정 파일
+}
