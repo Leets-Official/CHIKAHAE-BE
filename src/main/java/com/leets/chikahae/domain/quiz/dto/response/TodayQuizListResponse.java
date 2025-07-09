e6fcd67144bff1dbedcf283d39c90180dc66a081
hwangjiyoung02 <hjy020714@gmail.com>
2025-07-09 16:12:11 +0900

    feat: 퀴즈 list 반환 dto 구현

---
+++ src/main/java/com/leets/chikahae/domain/quiz/dto/response/TodayQuizListResponse.java
@@ -0,0 +1,16 @@
+package com.leets.chikahae.domain.quiz.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+public record TodayQuizListResponse(
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate quizDate,
+
+        List<QuizResponse> quizList
+) {
+}
