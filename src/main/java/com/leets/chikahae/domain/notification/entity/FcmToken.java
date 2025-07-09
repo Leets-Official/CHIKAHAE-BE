fd037248c79c698363c8f577bcb29e72c7bd0f5c
seokjun01 <suckjungod@naver.com>
2025-07-10 02:15:58 +0900

    fix: 연관관계 설정 (member)

--- src/main/java/com/leets/chikahae/domain/notification/entity/FcmToken.java
+++ src/main/java/com/leets/chikahae/domain/notification/entity/FcmToken.java
@@ -1,10 +1,15 @@
 package com.leets.chikahae.domain.notification.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Entity
 public class FcmToken extends BaseEntity {
 
 	@Id
@@ -12,8 +17,9 @@ public class FcmToken extends BaseEntity {
 	@Column(name = "fcm_token_id")
 	private Long tokenId;
 
-	@Column(name = "member_id", nullable = false)
-	private Long memberId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
 
 	@Column(name = "fcm_token", nullable = false)
 	private String fcmToken;
--- src/main/java/com/leets/chikahae/domain/notification/entity/NotificationSlot.java
+++ src/main/java/com/leets/chikahae/domain/notification/entity/NotificationSlot.java
@@ -8,9 +8,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 
@@ -24,8 +27,9 @@ public class NotificationSlot extends BaseEntity {
 	@Column(name = "slot_id" )
 	private Long slotId;
 
-	@Column(name = "member_id", nullable = false)
-	private Long memberId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = "slot_type", nullable = false)
