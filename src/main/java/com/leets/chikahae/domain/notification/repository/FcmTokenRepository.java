0ee3545f5e65ab38e08e4909354b9ae87aea8f4a
seokjun01 <suckjungod@naver.com>
2025-07-11 02:02:02 +0900

    add: JPA리포지토리 상속추가

--- src/main/java/com/leets/chikahae/domain/notification/repository/FcmTokenRepository.java
+++ src/main/java/com/leets/chikahae/domain/notification/repository/FcmTokenRepository.java
@@ -1,13 +1,15 @@
 package com.leets.chikahae.domain.notification.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.leets.chikahae.domain.notification.entity.FcmToken;
 
 @Repository
-public interface FcmTokenRepository {
+public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
 
 	//스케줄러에서 푸시 전ㄴ송 대상 토큰 수집 시에 사용
 	List<FcmToken> findByMember(Member member);
