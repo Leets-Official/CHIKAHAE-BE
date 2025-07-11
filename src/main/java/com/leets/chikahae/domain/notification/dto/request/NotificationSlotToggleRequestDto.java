9fe378d9fe55145d722e5ed8b86f6c4cdc109717
seokjun01 <suckjungod@naver.com>
2025-07-11 18:56:55 +0900

    feat: 알림슬롯 on/off 요청 dto구현

--- src/main/java/com/leets/chikahae/domain/notification/dto/request/NotificationSlotToggleRequestDto.java
+++ src/main/java/com/leets/chikahae/domain/notification/dto/request/NotificationSlotToggleRequestDto.java
@@ -1,4 +1,16 @@
 package com.leets.chikahae.domain.notification.dto.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// 슬롯 활성화/비활성 요청 Dto
+@Getter
+@NoArgsConstructor
 public class NotificationSlotToggleRequestDto {
+
+	private boolean enabled;
+
+	public static boolean toEnabled(NotificationSlotToggleRequestDto req) {
+		return req.enabled;
+	}
 }
