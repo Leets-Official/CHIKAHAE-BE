2e02895291499f71c1e0faa00d8d965d29e0c62e
seokjun01 <suckjungod@naver.com>
2025-07-11 19:00:06 +0900

    feat: 토큰 조회 응답dto 구현

--- src/main/java/com/leets/chikahae/domain/notification/dto/response/FcmTokenResponseDto.java
+++ src/main/java/com/leets/chikahae/domain/notification/dto/response/FcmTokenResponseDto.java
@@ -1,4 +1,18 @@
 package com.leets.chikahae.domain.notification.dto.response;
 
+import com.leets.chikahae.domain.notification.entity.FcmToken;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class FcmTokenResponseDto {
+
+	private final String token;
+
+	public static FcmTokenResponseDto from(FcmToken entity) {
+		return new FcmTokenResponseDto(entity.getFcmToken());
+	}
+
 }
