0aee75f14e01a5c14b14e631466c75cc9d2ba44f
seokjun01 <suckjungod@naver.com>
2025-07-11 18:59:36 +0900

    feat: 알림(슬롯) 시간 변경 요청 dto 구현

--- src/main/java/com/leets/chikahae/domain/notification/dto/request/NotificationSlotUpdateTimeRequestDto.java
+++ src/main/java/com/leets/chikahae/domain/notification/dto/request/NotificationSlotUpdateTimeRequestDto.java
@@ -1,4 +1,18 @@
 package com.leets.chikahae.domain.notification.dto.request;
 
+import java.time.LocalTime;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+//슬롯 발송 시간 변경 dto
+@Getter
+@NoArgsConstructor
 public class NotificationSlotUpdateTimeRequestDto {
+
+	private LocalTime sendTime;
+
+	public static LocalTime toLocalTime(NotificationSlotUpdateTimeRequestDto req) {
+		return req.sendTime;
+	}
 }
