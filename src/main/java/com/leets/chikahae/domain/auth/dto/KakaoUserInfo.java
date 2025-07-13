1a4aac03fa367f3cd48a065ffcb191fca53a88e4
hwangjiyoung02 <hjy020714@gmail.com>
2025-07-13 11:05:58 +0900

    feat: 로그인 인증 정보 PrincipalDetails에 등록하는 로직 구현

--- src/main/java/com/leets/chikahae/domain/auth/dto/KakaoUserInfo.java
+++ src/main/java/com/leets/chikahae/domain/auth/dto/KakaoUserInfo.java
@@ -1,6 +1,7 @@
 package com.leets.chikahae.domain.auth.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -11,14 +12,38 @@ public class KakaoUserInfo {
     @JsonProperty("kakao_account")
     private KakaoAccount kakaoAccount;
 
+    @Builder
+    public KakaoUserInfo(Long id, KakaoAccount kakaoAccount) {
+        this.id = id;
+        this.kakaoAccount = kakaoAccount;
+    }
+
     @Getter
+    @Builder
     public static class KakaoAccount {
         private String email;
         private Profile profile;
 
         @Getter
+        @Builder
         public static class Profile {
             private String nickname;
         }
+    }
+
+    public static KakaoUserInfo of(Long id, String email, String nickname) {
+        return KakaoUserInfo.builder()
+                .id(id)
+                .kakaoAccount(
+                        KakaoAccount.builder()
+                                .email(email)
+                                .profile(
+                                        KakaoAccount.Profile.builder()
+                                                .nickname(nickname)
+                                                .build()
+                                )
+                                .build()
+                )
+                .build();
     }
 }//class
