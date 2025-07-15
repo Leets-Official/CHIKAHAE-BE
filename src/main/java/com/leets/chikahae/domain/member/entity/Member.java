de7f5c999618e7de78acb12667a31f9c6d16e929
seokjun01 <suckjungod@naver.com>
2025-07-15 23:31:15 +0900

    fix: 빈칸 삭제

--- src/main/java/com/leets/chikahae/domain/member/entity/Member.java
+++ src/main/java/com/leets/chikahae/domain/member/entity/Member.java
@@ -1,24 +1,57 @@
 package com.leets.chikahae.domain.member.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "member")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Member {
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "member_id")
 	private Long memberId;
 
-	// 테스트용이므로 나머지 컬럼은 모두 생략해도 OK
-	public Member(Long memberId) {
-		this.memberId = memberId;
-	}
-}
+	@Column(name = "parent_id", nullable = false)
+	private Long parentId;
 
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "nickname", nullable = false, unique = true)
+	private String nickname;
+
+	@Column(name = "birth", nullable = false)
+	private LocalDate birth;
+
+	@Column(name = "profile_image")
+	private String profileImage;
+
+	@Column(name = "gender", nullable = false)
+	private Boolean gender;
+
+	@Column(name = "is_deleted", nullable = false)
+	private Boolean isDeleted;
+
+	@Column(name = "created_at", nullable = false,  updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at", nullable = false)
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+
+	// ✔️ AuthService에서 사용할 수 있도록 추가
+	public Long getId() {
+		return this.memberId;
+	}
+
+}//class
