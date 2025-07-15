03eaad312bc9d53e869d3fafba6bc97ff76a5627
seokjun01 <suckjungod@naver.com>
2025-07-15 21:36:03 +0900

    add: 빠진 메소드 추가 (ParentId 찾기)

--- src/main/java/com/leets/chikahae/domain/member/repository/MemberRepository.java
+++ src/main/java/com/leets/chikahae/domain/member/repository/MemberRepository.java
@@ -1,8 +1,17 @@
 package com.leets.chikahae.domain.member.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 import com.leets.chikahae.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-}
+	// ✅ 닉네임 중복 확인용
+	boolean existsByNickname(String nickname);
+
+	Optional<Member> findFirstByParentId(Long parentId);
+
+
+}//interface
